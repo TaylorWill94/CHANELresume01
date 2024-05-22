4550a9786d6a368a5cc66a016821a2f4d5f7b8f0
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-05-22 18:31:57 -0400

    Chanel resume first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,23 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>CHANEL</h1>
+        <h1>Taylor Williams</h1>
+        <h2>Why you should hire me?</h2>
+      </div>
+      <div className="section">
+        <div>
+          <h1>ME</h1>
+        </div>
+        <div>
+          <h1>YOU</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
