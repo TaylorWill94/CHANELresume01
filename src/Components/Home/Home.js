46fcf44aab17a0319957d1395eba411a772cfb35
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-05-22 19:48:53 -0400

    Adds canva letters

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,8 @@
 import "./Home.css";
 import c from "./c_letter.png";
+import h from "./h_letter.png";
+import a from "./a_letter.png";
+import n from "./n_letter.png";
 
 function Home() {
   return (
@@ -13,6 +16,10 @@ function Home() {
         <div>
           <h1>ME</h1>
           <img className="c" src={c} alt="the letter c" width="100" />
+          <img className="h" src={h} alt="the letter c" width="100" />
+          <img className="a" src={a} alt="the letter c" width="100" />
+          <img className="n" src={n} alt="the letter c" width="100" />
+          <img className="a" src={a} alt="the letter c" width="100" />
         </div>
         <div>
           <h1>YOU</h1>
