29952f0fffb8b475474007bd90ca9ee6b7008575
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-05-22 20:00:02 -0400

    Moves Chanel words

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,19 +10,27 @@ function Home() {
   return (
     <>
       <div className="home">
-        <h1>CHANEL</h1>
-        <h1>Taylor Williams</h1>
-        <h2>Why you should hire me?</h2>
-      </div>
-      <div className="section">
+        {/* <h1>CHANEL</h1> */}
         <div>
-          <h1>ME</h1>
           <img className="c" src={c} alt="the letter c" width="100" />
           <img className="h" src={h} alt="the letter c" width="100" />
           <img className="a" src={a} alt="the letter c" width="100" />
           <img className="n" src={n} alt="the letter c" width="100" />
           <img className="e" src={e} alt="the letter c" width="100" />
           <img className="l" src={l} alt="the letter c" width="100" />
+        </div>
+        <h1>Taylor Williams</h1>
+        <h2>Why you should hire me?</h2>
+      </div>
+      <div className="section">
+        <div>
+          <h1>ME</h1>
+          {/* <img className="c" src={c} alt="the letter c" width="100" />
+          <img className="h" src={h} alt="the letter c" width="100" />
+          <img className="a" src={a} alt="the letter c" width="100" />
+          <img className="n" src={n} alt="the letter c" width="100" />
+          <img className="e" src={e} alt="the letter c" width="100" />
+          <img className="l" src={l} alt="the letter c" width="100" /> */}
         </div>
         <div>
           <h1>YOU</h1>
